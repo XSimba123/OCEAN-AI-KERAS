9bac6ae965b1f2b9b5f6620e1db242221c7d5dcd
XSimba123 <40066081+XSimba123@users.noreply.github.com>
2019-07-30 17:19:41 +0800

    Add files via upload

--- data_IO/data_reader.py
+++ data_IO/data_reader.py
@@ -37,7 +37,7 @@ def parse_label_file(src_path, sep=' ', skip_header=False):
     return res
 
 
-def match_data_label(label_dict, imgs_dict):
+def match_data_label(label_dict, imgs_dict, aux_dict=None):
     imgs, aux_data = [], []
     p = []
     for f in os.listdir('/home/xujingning/ocean/ocean_data/problem/'):
@@ -50,7 +50,11 @@ def match_data_label(label_dict, imgs_dict):
                 imgs.append(imgs_dict[key])
             else:
                 print('%s not in image dict' % key)
-    return imgs
+            if aux_dict is not None and key in aux_dict:
+                aux_data.append(aux_dict[key])
+            elif aux_dict is not None:
+                print('%s not in aux data dict' % key)
+    return imgs, aux_data
 
 
 def convert_data_type(data, dtype=np.float32):
@@ -61,13 +65,24 @@ def convert_data_type(data, dtype=np.float32):
     return data.astype(dtype)
 
 
+def get_aux_data(data_path):
+    if data_path is None:
+        return None
+    raw_aux_data = {}
+    csv_data = read_csv(data_path)
+    for row in csv_data[1:]:
+        raw_aux_data[str(int(row[0]))] = [float(r) for r in row[1:]]
+    return raw_aux_data
+
 
 def get_data(data_path, label_path, aux_data_path=None):
     label_dict = parse_label_file(label_path, ',', True)
     imgs_dict = read_image(data_path)
-    imgs = match_data_label(label_dict, imgs_dict)
+    aux_data_dict = get_aux_data(aux_data_path)
 
+    imgs, aux_data = match_data_label(label_dict, imgs_dict, aux_data_dict)
     imgs = convert_data_type(imgs, np.float32)
+    aux_data = convert_data_type(aux_data, np.float32)
     labels = convert_data_type(label_dict.values(), np.int32)
 
     return labels, imgs, aux_data
